9e66069987d716d0ae7a0f2f81aa566a474378ef
JasserRahali <rahali.jasser@gmail.com>
2022-09-15 13:34:50 +0000

    Deploying to gh-pages from @ JasserRahali/JasserRahali.github.io@1fad5f2818c1d43926d5a38e3e768f970f45fd61 🚀

---
+++ _next/static/DScmAfASg-32oZRJd-4_N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-22829bd6de8a2201e1e7.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
