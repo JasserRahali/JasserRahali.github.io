1fad5f2818c1d43926d5a38e3e768f970f45fd61
uid1466 <jasser.rahali@focus-corporation.com>
2022-09-15 14:33:52 +0100

    some changes

--- portfolio.js
+++ portfolio.js
@@ -4,9 +4,9 @@ export const greetings = {
 	name: "Jasser Rahali",
 	title: "Hi all, I'm Jasser",
 	description:
-		"I'm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Next.js and Blockchain development on Ethereum, Solidity, Web3.js, Moralis, and Brownie Framework.",
+		"I'm a passionate Full Stack web developer having an experience of web applications with JAVA, Spring Boot  Angular, NodeJs and ExpressJs.",
 	resumeLink:
-		"https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing",
+		"#",
 };
 
 export const openSource = {
@@ -33,12 +33,12 @@ export const skillsSection = {
 			lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
 			skills: [
 				emoji(
-					"⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js"
+					"⚡ Building responsive Single-Page-Apps (SPA) & PWA in Angular9"
 				),
-				emoji("⚡ Building responsive static websites using Next.js"),
-				emoji(
-					"⚡ Building RESTful APIs in Django & Django REST Framework"
-				),
+				emoji("⚡ Building responsive full stack project with MEAN Stack")
+				// emoji(
+				// 	"⚡ Building RESTful APIs in Django & Django REST Framework"
+				// ),
 			],
 			softwareSkills: [
 				{
@@ -57,13 +57,16 @@ export const skillsSection = {
 				
 				{
 					skillName: "Typescript",
-					fontAwesomeClassname: "vscode-icons:file-type-light-typescript",
+					fontAwesomeClassname: "vscode-icons:file-type-typescript-official",
 				},
 				{
 					skillName: "NodeJs",
-					fontAwesomeClassname: "vscode-icons:node",
+					fontAwesomeClassname: "vscode-icons:file-type-node2",
 				},
-				,
+				{
+					skillName: "MongoDB",
+					fontAwesomeClassname: "vscode-icons:file-type-mongo",
+				},
 				{
 					skillName: "Angular",
 					fontAwesomeClassname: "vscode-icons:file-type-angular",
@@ -87,64 +90,8 @@ export const skillsSection = {
 			],
 		},
 		{
-			title: "Cloud Infra-Architecture",
-			lottieAnimationFile: "/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
-			skills: [
-				emoji("⚡ Experience of working on multiple cloud platforms"),
-				emoji(
-					"⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
-				),
-				emoji(
-					"⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions"
-				),
-			],
-			softwareSkills: [
-				// ? Check README To get icon details
-				{
-					skillName: "AWS",
-					fontAwesomeClassname: "logos:aws",
-				},
-				{
-					skillName: "Azure",
-					fontAwesomeClassname: "logos:microsoft-azure",
-				},
-				{
-					skillName: "Heroku",
-					fontAwesomeClassname: "logos:heroku-icon",
-				},
-				{
-					skillName: "PostgreSQL",
-					fontAwesomeClassname: "logos:postgresql",
-				},
-				{
-					skillName: "Github",
-					fontAwesomeClassname: "akar-icons:github-fill",
-				},
-				{
-					skillName: "Docker",
-					fontAwesomeClassname: "logos:docker-icon",
-				},
-				{
-					skillName: "Github Actions",
-					fontAwesomeClassname: "logos:github-actions",
-				},
-				{
-					skillName: "Cloudinary",
-					fontAwesomeClassname: "logos:cloudinary",
-				},
-				{
-					skillName: "Nginx",
-					fontAwesomeClassname: "logos:nginx",
-				},
-				{
-					skillName: "Sentry",
-					fontAwesomeClassname: "logos:sentry-icon",
-				},
-			],
-		},
-		{
-			title: "Blockchain",
-			lottieAnimationFile: "/lottie/skills/ethereum.json", // Path of Lottie Animation JSON File
+			title: "Software Development",
+			lottieAnimationFile: "/lottie/skills/software.json", // Path of Lottie Animation JSON File
 			skills: [
 				emoji(
 					"⚡ Experience in developing Smart Contract using Solidity & Ethereum"
@@ -164,12 +111,12 @@ export const skillsSection = {
 			],
 			softwareSkills: [
 				{
-					skillName: "Ethereum",
-					fontAwesomeClassname: "logos:ethereum",
+					skillName: "JAVA",
+					fontAwesomeClassname: "vscode-icons:file-type-java",
 				},
 				{
-					skillName: "Solidity",
-					fontAwesomeClassname: "logos:solidity",
+					skillName: "Spring Boot",
+					fontAwesomeClassname: "/public/img/icons/common/springboot.svg",
 				},
 				{
 					skillName: "Web3js",
@@ -185,6 +132,64 @@ export const skillsSection = {
 				},
 			],
 		},
+		{
+			title: "Cloud Infra-Architecture",
+			lottieAnimationFile: "/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji("⚡ Experience of working on multiple cloud platforms"),
+				emoji(
+					"⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
+				),
+				emoji(
+					"⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions"
+				),
+			],
+			softwareSkills: [
+				// ? Check README To get icon details
+				{
+					skillName: "AWS",
+					fontAwesomeClassname: "logos:aws",
+				},
+				// {
+				// 	skillName: "Azure",
+				// 	fontAwesomeClassname: "logos:microsoft-azure",
+				// },
+				// {
+				// 	skillName: "Heroku",
+				// 	fontAwesomeClassname: "logos:heroku-icon",
+				// },
+				{
+					skillName: "PostgreSQL",
+					fontAwesomeClassname: "logos:postgresql",
+				},
+				
+				{
+					skillName: "Docker",
+					fontAwesomeClassname: "logos:docker-icon",
+				},
+				{
+					skillName: "Github",
+					fontAwesomeClassname: "akar-icons:github-fill",
+				},
+				{
+					skillName: "Github Actions",
+					fontAwesomeClassname: "logos:github-actions",
+				},
+				// {
+				// 	skillName: "Cloudinary",
+				// 	fontAwesomeClassname: "logos:cloudinary",
+				// },
+				{
+					skillName: "Nginx",
+					fontAwesomeClassname: "logos:nginx",
+				},
+				// {
+				// 	skillName: "Sentry",
+				// 	fontAwesomeClassname: "logos:sentry-icon",
+				// },
+			],
+		},
+		
 	],
 };
 
@@ -239,7 +244,7 @@ export const experience = [
 	{
 		role: "Full Stack Engineer",
 		company: "iXnihilo SAS , Tunisia",
-		companylogo: "/img/icons/common/dusecaSoftware.jpg",
+		companylogo: "/img/icons/common/xnihilo.png",
 		date: "Mar 2021 – Nov 2021",
 		desc: "I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.",
 		// descBullets: [
@@ -265,32 +270,20 @@ export const projects = [
 	},
 ];
 
-// export const feedbacks = [
-// 	{
-// 		name: "John Smith",
-// 		feedback:
-// 			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
-// 	},
-// 	{
-// 		name: "John Smith",
-// 		feedback:
-// 			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
-// 	},
-// ];
 
 // See object prototype on SEO.jsx page
 export const seoData = {
 	title: "Jasser RAHALI",
 	description:
-		"A passionate Full Stack Web Developer and Blockchain Developer.",
+		"A passionate Full Stack Web and Software Developer .",
 	author: "Jasser RAHALI",
-	image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+	image: "/public/img/icons/common/me.jpg",
 	url: "https://jasser-rahali.me/",
 	keywords: [
 		"Jasser",
 		"Jasser RAHALI",
-		"@1hanzla100",
-		"1hanzla100",
+		"@jasser.rahali",
+		"tn.anonyme",
 		"Portfolio",
 		"Jasser Portfolio ",
 		"Jasser RAHALI Portfolio",
